bdaa71a76bd997a86bd9abc0d9bd2f6b6fffd5ec
user <user@user-PC>
2018-06-18 17:04:18 +0400

    новая задача.

---
+++ src/massiv/Massiv.java
@@ -0,0 +1,21 @@
+/*создать массив размера n массив будет размера n. заполнить числами случайными от 0 до 100. вывести их на экран в порядке возрастания.
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package massiv;
+
+/**
+ *
+ * @author user
+ */
+public class Massiv {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+    
+}
